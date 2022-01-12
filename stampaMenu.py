1dab960cbf30424c84252ba27a1a4133022ad93d
mbiella <maurizio.biella@gmail.com>
2022-01-12 16:15:43 +0100

    ver 1.1.0

--- stampaMenu.py
+++ stampaMenu.py
@@ -1,11 +1,20 @@
-def printMenu():
-    print()
-    print('MENU')
-    print('-----------------------')
-    print('1 - Vedi Ingredienti')
-    print()
-    print('-----------------------')
-    print('Scegli 1-1, e per uscire')
-    option = input()
+import os
 
-    return option
+def printMenu(cur):
+
+    choice=-1
+    while(choice !='e'):
+        print()
+        print('MENU')
+        print('-----------------------')
+        print('1 - Vedi Ingredienti')
+        print()
+        print('-----------------------')
+        print('Scegli 1-1, e per uscire')
+        choice = input()
+        os.system('cls')
+        if(choice=='e'):
+            exit()
+        if(choice=='1'):
+            for row in cur.execute('SELECT i.ID, i.name, t.typology  FROM ingredients as i INNER JOIN typology as t on i.ID=t.ID'):
+                print(row)
